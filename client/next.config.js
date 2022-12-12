a1646cd4f8d77cd35f176cbc1fed5663081d7ca9
Elvis Wei <ewei068@berkeley.edu>
2022-12-11 18:07:18 -0800

    github actions deployment

---
+++ client/next.config.js
@@ -0,0 +1,11 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
