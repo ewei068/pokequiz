2e442158c2f3aa7df326034d91baafd45bbe6562
Elvis Wei <ewei068@berkeley.edu>
2023-01-03 18:06:58 -0600

    finish stat quiz

--- client/src/components/DisplayStats.js
+++ client/src/components/DisplayStats.js
@@ -8,8 +8,8 @@ const DisplayStats = ({ id, pokemonData }) => {
       <StatBar label="HP" value={pokemonData[id]["stats"][0]} maxValue={MAX_VALUE} />
       <StatBar label="Attack" value={pokemonData[id]["stats"][1]} maxValue={MAX_VALUE} />
       <StatBar label="Defense" value={pokemonData[id]["stats"][2]} maxValue={MAX_VALUE} />
-      <StatBar label="Sp. Attack" value={pokemonData[id]["stats"][3]} maxValue={MAX_VALUE} />
-      <StatBar label="Sp. Defense" value={pokemonData[id]["stats"][4]} maxValue={MAX_VALUE} />
+      <StatBar label="Sp. Atk." value={pokemonData[id]["stats"][3]} maxValue={MAX_VALUE} />
+      <StatBar label="Sp. Def." value={pokemonData[id]["stats"][4]} maxValue={MAX_VALUE} />
       <StatBar label="Speed" value={pokemonData[id]["stats"][5]} maxValue={MAX_VALUE} />
     </>
   );
--- client/src/components/StatBar.js
+++ client/src/components/StatBar.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const StatBar = ({ label, value, maxValue }) => {
   return (
-    <>
-      {label } :
+    <div className="stat-container">
+      <div className="stat-label">{label}:</div>
       <div className="stat-bar">
         <div className="stat-bar-fill" style={{ width: `${(value / maxValue) * 100}%` }}>
           {value}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
